dccd87e7b7f08f727874d4e8f558fbb6113beca8
DarkKnight3089 <prajjwaltyagi3089555@gmail.com>
2020-04-19 17:07:35 +0530

    loginUi

--- app/src/main/java/com/example/instalearning/MainActivity.java
+++ app/src/main/java/com/example/instalearning/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.instalearning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view=this.getWindow().getDecorView();
+        view.setBackgroundResource(R.color.darkolivegreen);
+        EditText emailEditText=(EditText)findViewById(R.id.emailEditText);
+        EditText passwordEditText=(EditText)findViewById(R.id.passwordEditText);
+        Button loginButton=(Button)findViewById(R.id.loginButton);
+        ImageButton  imageButton=(ImageButton)findViewById(R.id.imageButton1);
     }
 }
